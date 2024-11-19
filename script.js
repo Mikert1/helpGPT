09b8bf897a6874f6919b9013b1a8d3bc32c7df7a
Mike Balvert <mikebalvert@icloud.com>
2024-11-19 11:21:13 +0100

    print raw data to html page

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello, world!');
+async function getData(dataType) {
+    const response = await fetch('http://127.0.0.1:8000/prompt_fragments/');
+    const data = await response.json();
+    return data;
+}
+
+const output = document.getElementById('output');
+
+getData('prompt_fragments').then(data => {
+    data.forEach(miniData => {
+        const div = document.createElement('div');
+        div.innerHTML = miniData.content;
+        output.appendChild(div);
+    });
+});
