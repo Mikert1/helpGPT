4bc9f67378f1d6135976f60c0eb23c65f726ca25
Mike Balvert <mikebalvert@icloud.com>
2024-11-22 12:14:23 +0100

    change profile visualy?

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const SelectedID = 1;
+
 async function getData(dataType) {
     try {
         const response = await fetch(`http://127.0.0.1:8000/${dataType}/`);
@@ -15,7 +17,7 @@ async function getData(dataType) {
             d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z" />
         <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
     </svg>
-    <h5>Instal Docker first see readme for more info!</h5>
+    <h5>The saved cards only work when docker is installed. For more info and containers read the README.md</h5>
     </div>`;
         return null;
     }
@@ -187,10 +189,17 @@ async function send() {
     createCards();
 }
 
+const dropdown = document.querySelector('.dropdown-content');
 // JavaScript to handle the dropdown toggle on click (if you want to allow toggling)
 document.querySelector('.profile-dropdown').addEventListener('click', function (e) {
-    const dropdown = this.querySelector('.dropdown-content');
     dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+    const options = dropdown.querySelectorAll('.profile')
+    options.forEach(option => {
+        option.addEventListener('click', function () {
+            const profileIcon = document.querySelector('.profile-icon');
+            profileIcon.src = option.querySelector('img').src;
+        });
+    });
 
     // Prevent click event from propagating to body to avoid immediate closing when clicking inside dropdown
     e.stopPropagation();
@@ -198,6 +207,5 @@ document.querySelector('.profile-dropdown').addEventListener('click', function (
 
 // Close dropdown if clicked outside
 document.addEventListener('click', function () {
-    const dropdown = document.querySelector('.dropdown-content');
     dropdown.style.display = 'none';
 });
